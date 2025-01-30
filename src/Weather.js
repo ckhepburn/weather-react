3b030fffbe385902a8adf77342bea97cc19d165b
ckhepburn <162160743+ckhepburn@users.noreply.github.com>
2025-01-30 09:52:36 -0500

    Added Bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -13,72 +13,74 @@ export default function Weather() {
 
   return (
     <div className='Weather'>
-      <form className='mb-3'>
-        <div className='row'>
-          <div className='col-9'>
-            <input
-              type='search'
-              placeholder='Type a city..'
-              className='form-control'
-              autoComplete='off'
-            />
-          </div>
-          <div className='col-3'>
-            <input
-              type='submit'
-              value='Search'
-              className='btn btn-primary w-100'
-            />
-          </div>
-        </div>
-      </form>
-      <div className='overview'>
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>Last updated: {weatherData.date}</li>
-          <li>{weatherData.description}</li>
-        </ul>
-      </div>
-      <div className='row'>
-        <div className='col-6'>
-          <div className='clearfix weather-temperature'>
-            <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
-              className='float-left'
-            />
-            <div className='float-left'>
-              <strong>{weatherData.temperature}</strong>
-              <span className='units'>
-                <a href='/'>°C</a> | <a href='/'>°F</a>
-              </span>
+      <div className='container'>
+        <form className='mb-3'>
+          <div className='row'>
+            <div className='col-9'>
+              <input
+                type='search'
+                placeholder='Type a city..'
+                className='form-control'
+                autoComplete='off'
+              />
+            </div>
+            <div className='col-3'>
+              <input
+                type='submit'
+                value='Search'
+                className='btn btn-primary w-100'
+              />
             </div>
           </div>
-        </div>
-        <div className='col-6'>
+        </form>
+        <div className='overview'>
+          <h1>{weatherData.city}</h1>
           <ul>
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind} km/h</li>
+            <li>Last updated: {weatherData.date}</li>
+            <li>{weatherData.description}</li>
           </ul>
         </div>
+        <div className='row'>
+          <div className='col-6'>
+            <div className='clearfix weather-temperature'>
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className='float-left'
+              />
+              <div className='float-left'>
+                <strong>{weatherData.temperature}</strong>
+                <span className='units'>
+                  <a href='/'>°C</a> | <a href='/'>°F</a>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className='col-6'>
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
+        </div>
+        <footer>
+          This project was coded by Carolyn Thomas for{' '}
+          <a href='https://www.shecodes.io/' target='blank'>
+            SheCodes
+          </a>{' '}
+          and is open-sourced on{' '}
+          <a href='https://github.com/ckhepburn/weather-react' target='blank'>
+            GitHub
+          </a>{' '}
+          and hosted on{' '}
+          <a
+            href='https://app.netlify.com/sites/comfy-choux-9204e2/overview'
+            target='blank'
+          >
+            Netlify
+          </a>
+        </footer>
       </div>
-      <footer>
-        This project was coded by Carolyn Thomas for{' '}
-        <a href='https://www.shecodes.io/' target='blank'>
-          SheCodes
-        </a>{' '}
-        and is open-sourced on{' '}
-        <a href='https://github.com/ckhepburn/weather-react' target='blank'>
-          GitHub
-        </a>{' '}
-        and hosted on{' '}
-        <a
-          href='https://app.netlify.com/sites/comfy-choux-9204e2/overview'
-          target='blank'
-        >
-          Netlify
-        </a>
-      </footer>
     </div>
   );
 }
