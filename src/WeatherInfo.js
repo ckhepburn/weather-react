eacbbff9a0558d0ab64f9026dd0ceda7156c875b
ckhepburn <162160743+ckhepburn@users.noreply.github.com>
2025-01-31 16:44:47 -0500

    Update Temperature to show C and F

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormattedDate from './FormatedDate';
+import WeatherTemperature from './WeatherTemperature';
 
 export default function WeatherInfo(props) {
   return (
@@ -22,10 +23,7 @@ export default function WeatherInfo(props) {
               className='float-left'
             />
             <div className='float-left'>
-              <strong>{Math.round(props.data.temperature)}</strong>
-              <span className='units'>
-                <a href='/'>°C</a> | <a href='/'>°F</a>
-              </span>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
